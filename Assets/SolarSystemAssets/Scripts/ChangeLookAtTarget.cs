c84726e23f96fc21abf9d81b14bd2becfc9ce940
JMLOSP <jmlo@dbsolid.es>
2016-02-07 21:09:22 +0100

    Commit Inicial

---
+++ Assets/SolarSystemAssets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour
+{
+  public GameObject target; // the target that the camera should look at
+
+  void Start()
+  {
+    //Si target es null generamos
+    if (target != null)
+      return;
+
+    target = this.gameObject;
+    Debug.Log("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+  }
+
+  // Called when MouseDown on this gameObject
+  void OnMouseDown()
+  {
+    // change the target of the LookAtTarget script to be this gameobject.
+    LookAtTarget.target = target;
+    Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+  }
+}
---
+++ Assets/SolarSystemAssets/Scripts/LookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTarget : MonoBehaviour
+{
+  public static GameObject target; // the target that the camera should look at
+
+  void Start()
+  {
+    if (target != null)
+      return;
+
+    target = this.gameObject;
+    Debug.Log("LookAtTarget target not specified. Defaulting to parent GameObject");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (target)
+      transform.LookAt(target.transform);
+  }
+}
